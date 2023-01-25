092d8aa56326e564278e236b824593d115e3fe93
bazylcossac <102479081+bazylcossac@users.noreply.github.com>
2023-01-25 23:13:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let number = document.getElementById("number")
+let number2 = document.getElementById("number2")
+let background = document.querySelector("score")
+let background2 = document.querySelector("score2")
+let score = 0
+let score2 = 0
+
+function add1(){
+    score = score + 1
+    number.innerHTML = score   
+    
+}
+function add3(){
+    score = score + 3
+    number.innerHTML = score
+    
+}
+function add5(){
+    score = score + 5
+    number.innerHTML = score
+    
+}function add1s(){
+    score2 +=1
+    number2.innerHTML = score2
+      
+}
+function add3s(){
+    score2 += 3
+    number2.innerHTML = score2
+    
+}
+function add5s(){
+    score2 +=5
+    number2.innerHTML = score2
+    
+}
+function resets(){
+    score = 0
+    score2 = 0
+    number2.innerHTML = score2
+    number.innerHTML = score
+}
+
